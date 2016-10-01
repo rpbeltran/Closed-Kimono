5dbf6aa449888d5ac1a39b1f692531ab228fdbe6
Brian Coombs <bjcoombs1@gmail.com>
2016-10-01 17:47:21 -0500

    Login Page

---
+++ index.js
@@ -0,0 +1,6 @@
+$("#login").on('click', function(){
+	window.alert(document.getElementById('email').value);
+	window.alert(document.getElementById('password1').value);
+	window.alert(document.getElementById('password2').value);
+	
+});
