a023888cfdc78d132af3d19352f011f25a0900b1
Tristan Seifert <t@tseifert.me>
2016-10-02 00:40:44 -0500

    ass

--- crypto/build-linux.sh
+++ crypto/build-linux.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # who the fuck needs makefiles lmao
 
-clang -dynamiclib -lcrypto -std=c99 cryptoshit.c -fvisibility=hidden -o libcryptoshit.dylib
+gcc -dynamiclib -lcrypto -std=c99 cryptoshit.c -fvisibility=hidden -o libcryptoshit.dylib
 
-
-clang -lcrypto -std=c99 cryptoshit-test.c -L. -lcryptoshit -o cryptoshit-test
+gcc -lcrypto -std=c99 cryptoshit-test.c -L. -lcryptoshit -o cryptoshit-test
 ./cryptoshit-test
